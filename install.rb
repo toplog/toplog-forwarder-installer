6e91fa92fdec800c61467f38c254ec8fe26c9ef2
cameronevans <cameron@toplog.ca>
2014-10-08 16:54:18 -0300

    added reinstall and change config option

--- install.rb
+++ install.rb
@@ -94,7 +94,80 @@ def uninstall_forwarder(distrib)
 
 	`sudo rm /etc/init.d/toplog-forwarder`
 	`sudo rm -rf /usr/bin/toplog/`
+	puts "Successfully uninstalled TopLog Logstash-Forwarder uploader"
 
+end
+
+def change_config
+	# get auth token
+	puts "Please enter your authentication token:"
+	token = gets.chomp
+
+	#get user's log types
+	endpoint = "http://#{$toplog_server}/configurations?access_token=#{token}"
+	types = request_toplog(endpoint, 'get')
+
+	#prompt to select type
+	puts "You have created the following log types:"
+	types.each { |id, name| puts "#{id}: #{name}\n" }
+	type_selected = false
+
+	until type_selected
+		puts "Please enter the corresponding id number of the log type you wish to forward"
+		user_type_id = gets.chomp
+
+		if user_type_id =~ /\A\d+\z/ and types.has_key?(user_type_id)
+			type_selected = true
+		else
+			puts "Error, log type not found"
+		end
+	end
+
+	path_selected = false
+	puts "Please enter full path to the log file you wish to forward (example: /path/to/my.log)"
+	#get log path
+	until path_selected
+		path = Readline.readline("> ", true).rstrip
+		if File.exist?(path)
+			path_selected = true
+		else
+			puts "File not found, please try again"
+		end
+	end
+
+	puts "Please enter a name for your stream:"
+	stream_name = gets.chomp
+
+	#create stream, get config json
+	endpoint = URI.escape("http://#{$toplog_server}/streams?access_token=#{token}&configuration_id=#{user_type_id}&name=#{stream_name}")
+	response = request_toplog(endpoint, 'post')
+
+	if response['success']
+		#replace path in config
+		response['config']['files'].each do |file|
+			file['paths'] = [path]
+			file['key'] = token
+		end
+
+		#install forwarder
+		return JSON.pretty_generate(response['config'])
+
+	else
+		puts "Could not create stream, please try again"
+		exit
+	end
+
+end
+
+def check_installed(required)
+	installed = File.directory?('/usr/bin/toplog/logstash-forwarder')
+	if installed and !required
+		puts "It appears the TopLog Forwarder is already installed, please run 'sudo bash install.sh -h' for a list of command args"
+		exit
+	elsif !installed and required
+		puts "It appears the TopLog Forwarder is not installed, please run 'sudo bash install.sh -h' for a list of command args"
+		exit
+	end
 end
 
 #get package based on distrib
@@ -103,81 +176,36 @@ if system( "which dpkg >/dev/null 2>/dev/null" )
 else
 	distrib = 'redhat'
 end
-
-case ARGV[0]
-when '-u'
-	uninstall_forwarder(distrib)
-	exit
-when '-r'
-	task = reinstall
-when '-c'
-	task = change_config
-when '-h', '--help'
-	puts "[-r] Reinstall TopLog Logstash-Forwarder"
-	puts "[-u] Uninstall TopLog Logstash-Forwarder"
-	puts "[-c] Change uploader configuration"
-	puts "[-h] or [--help] List install.sh command args"
-	exit
-else
-	puts "Invalid argument #{ARGV[0]}\nPlease enter 'sudo ruby install.rb -h' to see full list of possible command arguments"
-	exit
-end
-
-# get auth token
-puts "Please enter your authentication token:"
-token = gets.chomp
-
-#get user's log types
-endpoint = "http://#{$toplog_server}/configurations?access_token=#{token}"
-types = request_toplog(endpoint, 'get')
-
-#prompt to select type
-puts "You have created the following log types:"
-types.each { |id, name| puts "#{id}: #{name}\n" }
-type_selected = false
-
-until type_selected
-	puts "Please enter the corresponding id number of the log type you wish to forward"
-	user_type_id = gets.chomp
-
-	if user_type_id =~ /\A\d+\z/ and types.has_key?(user_type_id)
-		type_selected = true
+if !ARGV[0].nil?
+	case ARGV[0]
+	when '-u'
+		check_installed(true)
+		uninstall_forwarder(distrib)
+	when '-r'
+		check_installed(true)
+		uninstall_forwarder(distrib)
+		config = change_config
+		install_forwarder(distrib, config)
+	when '-c'
+		check_installed(true)
+		change_config
+		`sudo service logstash-forwarder restart`
+		puts "Successfully updated TopLog's Logstash-Forwarder config, please check /usr/bin/toplog/logs/logstash-forwarder.log to confirm"
+	when '-h', '--help'
+		puts "[-r] Reinstall TopLog Logstash-Forwarder"
+		puts "[-u] Uninstall TopLog Logstash-Forwarder"
+		puts "[-c] Change uploader configuration"
+		puts "[-h] or [--help] List install.sh command args"
+		exit
 	else
-		puts "Error, log type not found"
+		puts "Invalid argument #{ARGV[0]}\nPlease enter 'sudo ruby install.rb -h' to see full list of possible command arguments"
+		exit
 	end
-end
-
-path_selected = false
-puts "Please enter full path to the log file you wish to forward (example: /path/to/my.log)"
-#get log path
-until path_selected
-	path = Readline.readline("> ", true).rstrip
-	if File.exist?(path)
-		path_selected = true
-	else
-		puts "File not found, please try again"
-	end
-end
-
-puts "Please enter a name for your stream:"
-stream_name = gets.chomp
-
-#create stream, get config json
-endpoint = URI.escape("http://#{$toplog_server}/streams?access_token=#{token}&configuration_id=#{user_type_id}&name=#{stream_name}")
-response = request_toplog(endpoint, 'post')
-
-if response['success']
-	#replace path in config
-	response['config']['files'].each do |file|
-		file['paths'] = [path]
-		file['key'] = token
-	end
-
-	#install forwarder
-	install_forwarder(distrib, JSON.pretty_generate(response['config']))
-
 else
-	puts "Could not create stream, please try again"
-	exit
+	check_installed(false)
+	config = change_config
+	install_forwarder(distrib, config)
+
 end
+
 
